78fd05c7bf5542597ecff41352327afab6b29ab4
IsisAriele <isisariele1@gmail.com>
2023-12-18 23:56:35 -0300

    Adicionando configurações para o JWT funcionar

--- app/application/urls.py
+++ app/application/urls.py
@@ -1,9 +1,15 @@
 from application.views.user_views import UserViewSet
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path(
         "users",
-        UserViewSet.as_view({"post": "update_or_create", "put": "update_or_create"}),
+        UserViewSet.as_view(
+            {"post": "update_or_create", "put": "update_or_create"},
+            name="users_update_or_create",
+        ),
     ),
+    path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
